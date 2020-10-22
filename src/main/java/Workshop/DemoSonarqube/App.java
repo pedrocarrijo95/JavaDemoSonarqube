be8b73d9d31c0e2eb83f5423d5e5e43d16824127
pedrocarrijo <pedro.carrijo@oracle.com>
2020-10-22 16:47:16 -0300

    first

--- src/main/java/Workshop/DemoSonarqube/App.java
+++ src/main/java/Workshop/DemoSonarqube/App.java
@@ -1,13 +1,25 @@
 package Workshop.DemoSonarqube;
-
+import java.util.Calendar;
+import java.util.Arrays;
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        coletarInfo("pedro","19");
+        
+        
+        
+    }
+    
+    
+    
+    public void coletarInfo(String nome, int Idade) {
+    	System.out.println("Olá "+nome+" Sua idade é: "+idade);
     }
 }
